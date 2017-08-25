7aece79690450f56a0e6e287e04aa86efbc4af22
George Moura <gwmoura@gmail.com>
2017-08-25 10:14:52 -0300

    add test for score

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,4 @@
+from django.utils.timezone import now
 from django.test import TestCase
 
 from .models import Theme, Video, Thumb
@@ -5,10 +6,12 @@ from .models import Theme, Video, Thumb
 class VideosTests(TestCase):
     def setUp(self):
         self.theme = Theme.objects.create(name='Games')
-        self.video = Video.objects.create(title='God Of War III', date_uploaded='2017-02-01', views=100)
+        self.video = Video.objects.create(title='God Of War III', date_uploaded=now(), views=100)
         self.video.themes.add(self.theme)
-        self.thumb_like = Thumb.objects.create(is_positive=True, time='2017-02-01 05:20', video=self.video)
-        self.thumb_unlike = Thumb.objects.create(is_positive=False, time='2017-02-01 05:27', video=self.video)
+        self.thumb_like = Thumb.objects.create(is_positive=True, time=now(), video=self.video)
+        self.thumb_unlike = Thumb.objects.create(is_positive=False, time=now(), video=self.video)
 
     def test_count_thumbs(self):
         self.assertEquals(2, self.video.thumbs.count())
+        self.assertEquals(0, self.video.comments.count())
+        self.assertEquals(15.0, self.video.score)
